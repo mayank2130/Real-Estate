298d45914bcd74b3983ce208ef6f5e09e5fe40fd
mayank2130 <mayankthakur1712@gmail.com>
2024-01-05 23:36:52 +0530

    Logic for User Sign In

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,35 @@
 import User from "../models/userModel.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password,10)
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ userName, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({message: "User Created Successfully"})
-    
+    res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not Found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "Wrong email or password!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
