18bd5d82a282179f675bc9a8f381a27b3bd97b76
mayank2130 <mayankthakur1712@gmail.com>
2024-01-05 23:55:37 +0530

    Adding Sign in Functionality and UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate("/")
+    } catch (error) {
+      setLoading(false);
+      setError(error.message)
+    }
+  };
+
   return (
-    <div className='text-center mt-28 font-bold text-2xl'>Welcome to Mayank Estates</div>
-  )
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-3xl text-center my-10">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
+        <button
+          disabled={loading}
+          className="bg-red-600 p-3 font-semibold text-white rounded-lg uppercase hover:opacity-90  disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex mt-4 gap-3">
+        <p>Dont Have an Account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-600 font-medium ">Sign Up</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
