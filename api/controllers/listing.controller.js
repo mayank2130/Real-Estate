9108e93c7a9b73db1f88f5b57a9766ab5079c7a1
mayank2130 <mayankthakur1712@gmail.com>
2024-01-10 19:43:49 +0530

    create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -4,25 +4,45 @@ import { errorHandler } from "../utils/error.js";
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
-    return res.status(201).json(listing)
+    return res.status(201).json(listing);
   } catch (error) {
     next(error);
   }
 };
 
 export const deleteListing = async (req, res, next) => {
-  const listing = await Listing.findById(req.params.id)
+  const listing = await Listing.findById(req.params.id);
 
-  if(!listing) {
-    return next(errorHandler(404, "Listing Not Found!"))
+  if (!listing) {
+    return next(errorHandler(404, "Listing Not Found!"));
   }
-  if(req.user.id !== listing.userRef) {
-    return next(errorHandler(401, "You can only delete your own listings!"))
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
   }
   try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json("Listing deleted successfully!")
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing deleted successfully!");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only edit your own listings!"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
