1ddddab1677095c4c98b69a797ad3ac30c56553b
mayank2130 <mayankthakur1712@gmail.com>
2024-01-11 08:37:02 +0530

    e4

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -79,7 +79,7 @@ export default function Home() {
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
                   backgroundSize: 'cover',
                 }}
-                className='h-[600px]'
+                className='h-[500px]'
                 
               ></div>
             </SwiperSlide>
